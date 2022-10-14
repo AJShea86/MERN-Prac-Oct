6b999b0a6fe4152f193ff88e2f85a1f03fcf0b11
AJ Shea <andrewsheamls@gmail.com>
2022-10-14 16:05:56 -0600

    cleaned up routes

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -1,21 +1,15 @@
 const express = require('express')
 const router = express.Router()
+const {
+    getGoals, 
+    setGoal, 
+    updateGoal, 
+    deleteGoal} = require('../controllers/goalController')
 
-router.get('/', (req, res) => {
-    res.status(200).json({message: 'Get Goals'})
-})
+router.route('/').get(getGoals).post(setGoal)
 
-router.post('/', (req, res) => {
-    res.status(200).json({message: 'Set Goals'})
-})
+router.route('/:id').put(updateGoal).delete(deleteGoal)
 
-router.put('/:id', (req, res) => {
-    res.status(200).json({message: `Update Goal ${req.params.id}`})
-})
-
-router.delete('/:id', (req, res) => {
-    res.status(200).json({message: `Delete Goal ${req.params.id}`})
-})
 
 
 module.exports = router
