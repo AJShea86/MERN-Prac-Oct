4f947c4040f6b4f8b765d25b12a254b61f202ab6
AJ Shea <andrewsheamls@gmail.com>
2022-10-19 19:49:05 -0600

    last time before deploying

--- backend/server.js
+++ backend/server.js
@@ -22,6 +22,8 @@ if(process.env.NODE_ENV === 'production'){
     app.use(express.static(path.join(__dirname, '../frontend/build')))
 
     app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '../', 'frontend', 'build', 'index.html' )))
+} else {
+    app.get('/', (res, res) => res.send('Please set to production'))
 }
 
 
